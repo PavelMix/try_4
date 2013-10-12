344535da286905de176e2990295260caf00eaaf2
PavelMix <pavel_mix@inbox.ru>
2013-10-12 15:20:41 +0300

    Install commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Try4::Application.config.secret_key_base = '3834372c1214767ad33ff3cfe1bb7ead7cca677475b4c252f69a38e375b57b418f4adac86504d966cc3f62a33a24038b27b394f3d219da986f388e554c965fc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Try4::Application.config.session_store :cookie_store, key: '_try_4_session'
